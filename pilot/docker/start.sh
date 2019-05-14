7d237e3e0ace647f9d9d4185c69b817a9bb7e515
harry ge <harryge@qq.com>
2019-05-14 16:42:47 +0800

    update start.sh

--- pilot/docker/start.sh
+++ pilot/docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "starting proxy_debug"
 chmod +777 /dev/stdout
-/usr/local/bin/istio-iptables.sh -p 15001 -u 1337 -m REDIRECT -i '*' -b "$INBOUND_PORTS"
+/usr/local/bin/istio-iptables.sh -p 15001 -u 1337 #-m REDIRECT -i '*' -b "$INBOUND_PORTS"
 iptables -t nat -I ISTIO_OUTPUT -d $HOST/32 -p tcp -m tcp --dport 5051 -j RETURN
 su istio-proxy -c "/usr/local/bin/pilot-agent proxy --discoveryAddress $DISCOVERY_ADDRESSS  --domain $SERVICES_DOMAIN --serviceregistry Mesos --serviceCluster $SERVICE_NAME --zipkinAddress $ZIPKIN_ADDRESS --configPath /var/lib/istio"
